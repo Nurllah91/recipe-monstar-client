0a889c352b33fa45b38fb71be64d0fd9a963c0a3
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-03 04:02:40 +0600

    favorite button disable functionality added

--- src/Components/Pages/Chef/Recipe.jsx
+++ src/Components/Pages/Chef/Recipe.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import './Recipe.css'
+import { FaRegHeart } from "react-icons/fa";
 const Recipe = ({ recipe }) => {
     const { recipe_name, rating, ingredients, cooking_method } = recipe;
+    const [mark, setMark] = useState(false)
+
+
+    const handleFavorite = () => {
+        setMark(true)
+    }
     return (
-        <div className="w-[80%] mx-auto mt-5 bg-pink-100 p-10 rounded-lg shadow-lg">
+        <div className="w-[80%] mx-auto mt-5 bg-pink-50 p-10 rounded-lg shadow-lg">
             <table>
                 <caption className="text-2xl font-bold text-purple-500 mb-4">{recipe_name}</caption>
                 <thead>
@@ -16,7 +24,7 @@ const Recipe = ({ recipe }) => {
                         <td>Ingredients</td>
                         <td>
                             {
-                                ingredients.map((item, index) =><li key={index}> {item}</li>)
+                                ingredients.map((item, index) => <li key={index}> {item}</li>)
                             }
                         </td>
                     </tr>
@@ -28,7 +36,17 @@ const Recipe = ({ recipe }) => {
                         <td>Ratings</td>
                         <td>{rating} star</td>
                     </tr>
-                   
+                    <tr>
+                        <td colSpan={2} className='text-center'><button
+                            onClick={handleFavorite}
+                            className='btn btn-secondary'
+                            disabled = {mark}
+                            >
+                            Favorite<FaRegHeart className='ml-2' />
+                        </button></td>
+
+                    </tr>
+
                 </tbody>
             </table>
 
