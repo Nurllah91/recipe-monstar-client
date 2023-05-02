7d3b944b6f29633750cfc52aec98294cd5b8d1de
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-02 09:25:03 +0600

    nav bar designed and router added

--- src/Components/Routs/Routs.jsx
+++ src/Components/Routs/Routs.jsx
@@ -1,15 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import About from "../Pages/About/About";
+import Blog from "../Pages/Blog/Blog";
+import Login from "../Pages/Login/Login";
+import Registration from "../Pages/Registration/Registration";
 
 
 
 
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>
-    },
-  ]);
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'blog',
+        element:<Blog></Blog>
+      },
+      {
+        path: 'login',
+        element:<Login></Login>
+      },
+      {
+        path: 'registration',
+        element:<Registration></Registration>
+      }
+    ]
 
-  export default router;
+  },
+]);
+
+export default router;
--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <div>
+        <div className="min-h[calc(100vh-62px)]">
             <footer className="footer p-10 bg-neutral text-neutral-content">
                 <div>
                     <span className="footer-title">Services</span>
