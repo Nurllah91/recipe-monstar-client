31a63bfa0c4bbd5a44d074139d8f05ee4baf51bd
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-04 05:11:34 +0600

    footer modified with link tag

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,27 +1,27 @@
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <div className="divide-y  relative bottom-0 left-0">
             <footer className="footer p-10 bg-neutral text-neutral-content">
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <span className="footer-title">Awards and certifications</span>
+                    <Link className="link link-hover">All certificate</Link>
+                    <Link className="link link-hover">Awards of chef&apos;s</Link>
+                    <Link className="link link-hover">Our client&apos;s feedback</Link>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <span className="footer-title">About Us</span>
+                    <Link className="link link-hover">Address</Link>
+                    <Link className="link link-hover">Contact with us</Link>
+                    <Link className="link link-hover">Chef&apos;s</Link>
+                    <Link className="link link-hover">Our Location</Link>
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <span className="footer-title">Disclaimer</span>
+                    <Link className="link link-hover">Our Terms of use</Link>
+                    <Link className="link link-hover">Privacy policy</Link>
+                    <Link className="link link-hover">Copyrights</Link>
                 </div>
             </footer>
         </div>
