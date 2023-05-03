b82fddfc811b9c3ca7de95a9a42c1f51dbbee47d
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-03 13:37:22 +0600

    about page designed

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -2,8 +2,18 @@
 
 const About = () => {
     return (
-        <div>
-            About page
+        <div className="w-11/12 lg:w-9/12 bg-pink-50 rounded-lg p-10 my-12 mx-auto">
+            <h3  className="text-4xl text-center mb-3 text-fuchsia-800 ">About us</h3>
+
+            <p>
+                Welcome to our website! We are a team of passionate food lovers who have come together to curate a collection of the world's best chefs. Our goal is to provide you with an extensive selection of culinary talent, all in one place.
+
+                Our team has scoured the globe to find the most talented chefs who specialize in [insert relevant cuisines]. We have handpicked each chef based on their expertise, creativity, and dedication to their craft.
+
+                Whether you're looking for a chef to cater your next event, or simply want to explore new culinary horizons, our collection has something for everyone. From Michelin-starred chefs to up-and-coming culinary stars, we offer a diverse range of talent to suit every taste and budget.
+
+                Thank you for visiting our website. We hope that our collection of chefs will inspire you to explore new flavors and cuisines. If you have any questions or feedback, please don't hesitate to contact us. We are always here to help.
+            </p>
         </div>
     );
 };
