89fcfb6c3917a382f51b0b024f86f899a510e12a
nurullah91 <mdnurullah.ptk@gmail.com>
2023-11-15 01:00:22 +0600

    transition problem solved

--- src/Components/Pages/Home/ChefCard/ChefCard.jsx
+++ src/Components/Pages/Home/ChefCard/ChefCard.jsx
@@ -21,12 +21,13 @@ const ChefCard = ({ chef }) => {
                         src={chef_image}
                         width="100%"
                         height="100%"
-                        className="h-full w-full object-cover group-hover:rotate-3 group-hover:scale-125 transition-scale duration-700"
+                        style={{transitionProperty:"all", transitionDuration: "700ms"}}
+                        className="h-full w-full object-cover group-hover:rotate-3 group-hover:scale-125"
 
                     />
                 </div>
 
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-[#002f35b4] group-hover:via-[#002f3578] group-hover:to-[#002f35b5]"></div>
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-[#002f35f5] group-hover:from-[#002f35b4] group-hover:via-[#002f3578] group-hover:to-[#002f35b5]"></div>
 
                 <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[50%] md:translate-y-[45%] group-hover:translate-y-0 transition-all duration-500">
                     <h3 className="text-2xl font-bold text-white group-hover:py-0">{name}</h3>
@@ -41,33 +42,6 @@ const ChefCard = ({ chef }) => {
 
                 </div>
             </div>
-
-
-
-            {/* <div className=" w-11/12 lg:w-9/12 mb-8 mx-auto card lg:card-side bg-pink-100 shadow-xl">
-
-
-                <div className="">
-                    <LazyLoadImage
-                        alt={'chef image'}
-                        effect="blur"
-                        src={chef_image}
-                        width="100%"
-                        height="100%"
-                        className="lg:rounded-r-none h-[22rem] rounded-lg w-full lg:w-[30rem]"
-
-                    />
-                </div>
-                <div className="card-body">
-                    <h2 className="card-title text-3xl font-bold">Name: {name}</h2>
-                    <p className="font-bold">Years of experience: {years_of_experience}</p>
-                    <p className="font-bold">Numbers of recipes: {recipes.length}</p>
-                    <p className="font-bold flex items-center"><FaHandPointRight className="text-rose-600 mx-2"></FaHandPointRight> Likes: {likes}k</p>
-                    <div className="card-actions justify-end">
-                        <Link to={`/chefs/${id}`}> <button className="btn bg-pink-500 border-0">View Recipes</button></Link>
-                    </div>
-                </div>
-            </div> */}
         </div>
     );
 };
